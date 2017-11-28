fd3bb802220689d1d6eac06d70c4de57cb124a8d
LARS BERG <larsgbr@gmail.com>
2017-11-28 14:08:47 -0600

    created the code that enables a user to access the blocked sites during non work hours

--- website_blocker.py
+++ website_blocker.py
@@ -1,6 +1,6 @@
 ''' 
 This application writes to the hosts file and blocks a user from being able to access sites which are listed
-in the website_list section during normal business hours..
+in the website_list section during normal business hours.
 '''
 
 import time
@@ -13,11 +13,12 @@ from datetime import datetime as dt
 '''
 hosts_path="/etc/hosts"
 redirect="127.0.0.1"
+# Add the list of websites to this list that you want to block a user from accessing
 website_list=["www.facebook.com","facebook.com","www.twitter.com","twitter.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
-        print("Working hours...")
+        print("Normal Business Hours...")
         with open(hosts_path,'r+') as file:
             content=file.read()
             for website in website_list:
@@ -25,6 +26,13 @@ while True:
                     pass
                 else:
                     file.write(redirect+" "+ website+"\n")
-    else:	
-        print("Fun hours...")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("After Work Hours...")
     time.sleep(5)
